1097db22359bdb8f2946b3e7699c6f603c70eee9
Jalol91 <jalolrakhmov@gmail.com>
2022-09-21 14:05:26 +0500

    login

--- pages/auth/forgot/index.jsx
+++ pages/auth/forgot/index.jsx
@@ -1,5 +1,31 @@
 const ForgotPassword = () => {
-	return <div>Enter</div>;
-};
+	return (
+		<section className='pwd-page section-b-space'>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-lg-6 m-auto'>
+						<h2>Forget Your Password</h2>
+						<form className='theme-form'>
+							<div className='form-row row'>
+								<div className='col-md-12'>
+									<input
+										type='text'
+										className='form-control'
+										id='email'
+										placeholder='Enter Your Email'
+										required
+									/>
+								</div>
+								<a href='#' className='btn btn-solid w-auto'>
+									Submit
+								</a>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default ForgotPassword;
+export default ForgotPassword
